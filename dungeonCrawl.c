12aba1092f5af4ee22af029b2e87f251beecc014
Guilherme Milhomem <antoniomilhomem06@gmail.com>
2025-05-07 16:05:27 -0300

    Primeiro commit (texto placeholder)

---
+++ dungeonCrawl.c
@@ -0,0 +1,48 @@
+//dunegon Crawler, um jogo em C
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+#include<stdbool.h>
+#include<unistd.h>
+#include<termios.h>
+
+//"Todos nós possuímos as mesmas 24 horas".
+//Texto de primeria commit (teste)
+
+int main(){
+
+//Menu principal
+int tecla;
+printf("________________________________________________________________\n");
+printf("|     #                                                        |\n");
+printf("|     #                                                        |\n");
+printf("|     #                                                        |\n");
+printf("|     #                                                        |\n");
+printf("|  ####    #    #   #         ####     ####     ####    #      |\n");
+printf("| #    #   #    #   #####    #    #   ######   #    #   #####  |\n");
+printf("| #    #   #    #   #    #   #    #   #        #    #   #    # |\n");
+printf("|  ####     ####    #    #    #####    ####     ####    #    # |\n");
+printf("|                              #  #                            |\n");
+printf("|                             #  #                             |\n");
+printf("|                            ####                              |\n");
+printf("|   ########                                 #                 |\n");
+printf("|  ##      ##                                #                 |\n");
+printf("| #          #                               #                 |\n");
+printf("| #                                          #                 |\n");
+printf("| #              #       ####    #   #   #   #                 |\n");
+printf("| #          #   ####   #    #   #   #   #   #                 |\n");
+printf("|  ##      ##    #      #    #   #  ###  #   #                 |\n");
+printf("|   ########     #       ######   ### ###     ####             |\n");
+printf("|                                                              |\n");
+printf("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+printf("\n");
+printf("\n");
+
+
+
+
+
+
+return 0;
+}
