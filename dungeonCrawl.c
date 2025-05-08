616fddf7eb5ab12362de9f80751c8c5234648201
Guilherme Milhomem <antoniomilhomem06@gmail.com>
2025-05-08 20:58:29 -0300

    Segundo commit (menu)

--- dungeonCrawl.c
+++ dungeonCrawl.c
@@ -1,48 +1,102 @@
-//dunegon Crawler, um jogo em C
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<time.h>
-#include<stdbool.h>
-#include<unistd.h>
-#include<termios.h>
+#include"global.c"  
 
-//"Todos nós possuímos as mesmas 24 horas".
-//Texto de primeria commit (teste)
+#define NUM_OPTIONS 5
+#define NEW_GAME 2
 
-int main(){
+int teste; 
 
-//Menu principal
-int tecla;
-printf("________________________________________________________________\n");
-printf("|     #                                                        |\n");
-printf("|     #                                                        |\n");
-printf("|     #                                                        |\n");
-printf("|     #                                                        |\n");
-printf("|  ####    #    #   #         ####     ####     ####    #      |\n");
-printf("| #    #   #    #   #####    #    #   ######   #    #   #####  |\n");
-printf("| #    #   #    #   #    #   #    #   #        #    #   #    # |\n");
-printf("|  ####     ####    #    #    #####    ####     ####    #    # |\n");
-printf("|                              #  #                            |\n");
-printf("|                             #  #                             |\n");
-printf("|                            ####                              |\n");
-printf("|   ########                                 #                 |\n");
-printf("|  ##      ##                                #                 |\n");
-printf("| #          #                               #                 |\n");
-printf("| #                                          #                 |\n");
-printf("| #              #       ####    #   #   #   #                 |\n");
-printf("| #          #   ####   #    #   #   #   #   #                 |\n");
-printf("|  ##      ##    #      #    #   #  ###  #   #                 |\n");
-printf("|   ########     #       ######   ### ###     ####             |\n");
-printf("|                                                              |\n");
-printf("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
-printf("\n");
-printf("\n");
+const char *options[NUM_OPTIONS] = {
+    "New Game", //0
+    "Load Game (placeholder)", //1
+    "Options", //2
+    "Credits", //3
+    "Exit", //4
+};
+#define NEW_GAME 2
+    
+    const char*new[NEW_GAME] = {
+    "Sim", //0
+    "Não", //1
+};
 
+void draw_menu(int selected) {
+    clear();  //somente unix
+    printw("===== dungeon Crawl =====\n\n");
+    printw("                                                 _________ \n");
+    printw("                                                |         |\n");
+    printw("                                                |  _____  |\n");
+    printw("                                                | |     | |\n");
+    printw("                                                | |     | |\n");
+    printw("                                                | |_____| |\n");
+    printw("                                                |         |\n");
+    printw("                                                |   ___   |\n");
+    printw("                                                |  |   |  |\n");
+    printw("                                                |  |   |  |\n");
+    printw("                                                |  |___|  |\n");
+    printw("                                                |_________|\n");
 
+    for (int i = 0; i < NUM_OPTIONS; ++i) {
+        if (i == selected) {
+            attron(A_REVERSE);  
+            printw("> %s <\n", options[i]);
+            attroff(A_REVERSE);
+        } else {
+            printw("  %s\n", options[i]);
+        }
+    }
 
+    refresh();  
+}
 
+int main() {
+    int selected = 0;
+    int ch;
 
+    initscr();          
+    cbreak();           
+    noecho();          
+    keypad(stdscr, TRUE); 
 
-return 0;
+    draw_menu(selected);
+
+    while (1) {
+        ch = getch();
+
+        switch (ch) {
+            case KEY_UP:  
+                selected = (selected - 1 + NUM_OPTIONS) % NUM_OPTIONS;
+                break;
+            case KEY_DOWN:  
+                selected = (selected + 1) % NUM_OPTIONS;
+                break;
+            case 10:  
+                clear();
+                printw("Você escolheu: %s\n", options[selected]);
+                if (selected == 4) {  
+                    printw("Saindo do jogo...\n");
+                    break;
+                }
+                if (selected == 0){
+                    printw("Certeza que desejas iniciar um novo erro?\n");
+                    refresh();
+                    getch();
+                    break;
+
+                }
+                if (selected != 0){
+                printw("Pressione qualquer tecla para voltar ao menu...");
+                refresh();
+                getch();
+                }
+        }
+
+        draw_menu(selected);
+
+        if (ch == 10 && selected == 4)  
+            break;
+    }
+
+    endwin();  
+
+    return 0;
 }
---
+++ global.c
@@ -0,0 +1,6 @@
+#include <ncurses.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include<stdbool.h>
